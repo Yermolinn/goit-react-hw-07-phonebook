0af1adc706008539a5dd623504fd56a46a19a42e
Yermolinn <111793261+Yermolinn@users.noreply.github.com>
2023-05-17 14:39:11 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,42 +1,42 @@
-// import { ThreeDots } from 'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
 
-// export const Loader = () => (
-//   <div
-//     style={{
-//       position: 'absolute',
-//       top: '50%',
-//       left: '50%',
-//       transform: 'translate(-50%,-50%)',
-//     }}
-//   >
-//     <ThreeDots
-//       height="80"
-//       width="80"
-//       radius="9"
-//       color="grey"
-//       ariaLabel="three-dots-loading"
-//       visible={true}
-//     />
-//   </div>
-// );
+export const Loader = () => (
+  <div
+    style={{
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%,-50%)',
+    }}
+  >
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="grey"
+      ariaLabel="three-dots-loading"
+      visible={true}
+    />
+  </div>
+);
 
-import Skeleton from 'react-loading-skeleton';
-import 'react-loading-skeleton/dist/skeleton.css';
+// import Skeleton from 'react-loading-skeleton';
+// import 'react-loading-skeleton/dist/skeleton.css';
 
-export const Loader = () => {
-  const isLoading = true; // Задайте це значення відповідно до вашої логіки завантаження
+// export const Loader = () => {
+//   const isLoading = true; // Задайте це значення відповідно до вашої логіки завантаження
 
-  return (
-    <div>
-      {isLoading ? (
-        <Skeleton count={3} /> // Кількість скелетонів, які ви хочете відобразити
-      ) : (
-        // Ваш вміст, який буде показуватись, коли завантаження виконане
-        <div>
-          <h1>Заголовок</h1>
-          <p>Текст</p>
-        </div>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {isLoading ? (
+//         <Skeleton count={3} /> // Кількість скелетонів, які ви хочете відобразити
+//       ) : (
+//         // Ваш вміст, який буде показуватись, коли завантаження виконане
+//         <div>
+//           <h1>Заголовок</h1>
+//           <p>Текст</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
